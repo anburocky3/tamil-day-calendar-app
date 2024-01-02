8b70aa8c42cf79d4488218d0b716ceac9001fcbe
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-01-02 19:08:27 +0530

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // base: '/YOUR_BASE/'
+  base: "/tamil-day-calendar-app/",
   //   build: {
   //     rollupOptions: {
   //       input: {
